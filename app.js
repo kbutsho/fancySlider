83c125011726e6eb4eb399ff2a571f89f998bc1a
Kbutsho <kbutsho@yahoo.com>
2021-02-14 20:37:03 +0600

    Fancy Slide

---
+++ app.js
@@ -0,0 +1,156 @@
+const imagesArea = document.querySelector('.images');
+const gallery = document.querySelector('.gallery');
+const galleryHeader = document.querySelector('.gallery-header');
+const searchBtn = document.getElementById('search-btn');
+const sliderBtn = document.getElementById('create-slider');
+const sliderContainer = document.getElementById('sliders');
+// selected image 
+let sliders = [];
+
+
+// If this key doesn't work
+// Find the name in the url and go to their website
+// to create your own api key
+const KEY = '15674931-a9d714b6e9d654524df198e00&q';
+
+// show images 
+const showImages = (images) => {
+  imagesArea.style.display = 'block';
+  gallery.innerHTML = '';
+  // show gallery title
+  galleryHeader.style.display = 'flex';
+  images.forEach(image => {
+    let div = document.createElement('div');
+    div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
+    div.innerHTML = ` <img class="img-fluid img-thumbnail imgArea" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
+    gallery.appendChild(div)
+    toggleSpinner(false);
+  })
+
+}
+const getImages = (query) => {
+  toggleSpinner(true);
+  fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
+    .then(response => response.json())
+    .then(data => showImages(data.hits))
+    .catch(err => console.log(err))
+  // .catch(error => displayError('Images Not found !! try again'));
+
+}
+
+let slideIndex = 0;
+const selectItem = (event, img) => {
+  var GetTotal = document.getElementById("totalImage").innerText;
+  var total = parseInt(GetTotal);
+  let element = event.target;
+  element.classList.add('added');
+  let item = sliders.indexOf(img);
+  if (item === -1) {
+    sliders.push(img);
+    SetTotal = total + 1;
+    document.getElementById("totalImage").innerText = SetTotal;
+  } else {
+    element.classList.remove('added');
+    sliders.pop(img);
+    SetTotal = total - 1;
+    document.getElementById("totalImage").innerText = SetTotal;
+  }
+}
+var timer
+const createSlider = () => {
+  // check slider image length
+  if (sliders.length < 2) {
+    alert('Select at least 2 image.')
+    return;
+  }
+  // crate slider previous next area
+  sliderContainer.innerHTML = '';
+  const prevNext = document.createElement('div');
+  prevNext.className = "prev-next d-flex w-100 justify-content-between align-items-center";
+  prevNext.innerHTML = ` 
+  <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
+  <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
+  `;
+
+  sliderContainer.appendChild(prevNext)
+  document.querySelector('.main').style.display = 'block';
+  // hide image aria
+  imagesArea.style.display = 'none';
+  var getDuration = document.getElementById('duration').value;
+  var duration = parseInt(getDuration);
+  if (duration < 0) {  // duration < 0 is correct (-) value but it's too slow. so I consider minimum 1 second.
+    alert("Duration Should not be Negative! \n Default Duration is 1000 ms!")
+    duration = document.getElementById('duration').value = 1000;
+  }
+  else{
+    duration = document.getElementById('duration').value;
+  }
+    
+  sliders.forEach(slide => {
+    let item = document.createElement('div')
+    item.className = "slider-item";
+    item.innerHTML = `<img class="w-100"
+    src="${slide}"
+    alt="">`;
+    sliderContainer.appendChild(item)
+
+  })
+  changeSlide(0)
+  timer = setInterval(function () {
+    slideIndex++;
+    changeSlide(slideIndex);
+  }, duration);
+}
+
+// change slider index 
+const changeItem = index => {
+  changeSlide(slideIndex += index);
+}
+
+// change slide item
+const changeSlide = (index) => {
+
+  const items = document.querySelectorAll('.slider-item');
+  if (index < 0) {
+    slideIndex = items.length - 1
+    index = slideIndex;
+  };
+
+  if (index >= items.length) {
+    index = 0;
+    slideIndex = 0;
+  }
+
+  items.forEach(item => {
+    item.style.display = "none"
+  })
+  items[index].style.display = "block"
+}
+searchBtn.addEventListener('click', function () {
+  document.querySelector('.main').style.display = 'none';
+  clearInterval(timer);
+  const search = document.getElementById('search');
+  getImages(search.value)
+  sliders.length = 0;
+})
+
+sliderBtn.addEventListener('click', function () {
+
+  createSlider()
+})
+// show spinner 
+const toggleSpinner = (show) => {
+  const spinner = document.getElementById("loadingSpinner");
+  if (show) {
+    spinner.classList.remove("d-none");
+  }
+  else {
+    spinner.classList.add("d-none");
+  }
+}
+document.getElementById("search").addEventListener("keypress", function (event) {
+  if (event.key == 'Enter') {
+    document.getElementById("search-btn").click();
+  }
+})
+// document.get
